69dea020492b0f01c8439bd8dce9857a93230f1a
wucangji <wucangji@gmail.com>
2015-09-12 18:06:18 -0400

    update cpu

--- SendInfo.py
+++ SendInfo.py
@@ -2,6 +2,8 @@
 
 import criotdm
 import ciotdm
+import psutil
+import threading
 
 # Set varibles
 httphost = "localhost"
@@ -11,8 +13,22 @@ rt_ae              = 2
 rt_container       = 3
 rt_contentInstance = 4
 
+def sendCon():
+    threading.Timer(1.0,sendCon).start()
+    cpu = psutil.cpu_percent(interval=None)
+    attr = conattr + ":" + '\"%s\"' %(cpu)
+    print conattr
+    conIn_resp = connect.create("InCSE1/TemContainer", rt_contentInstance, attr)
+    print conIn_resp.text
+
 connect = criotdm.connect_to_iotdm(httphost, httpuser, httppass, "http")
-attr = '"mni":6'
-container_resp = connect.create("InCSE1", rt_container, attr, "TemContaienr")
+attr = '"mni":1'
+#attr=""
+container_resp = connect.create("InCSE1", rt_container, attr, "TemContainer")
 print(container_resp.text)
+
+conattr = '"con"'
+
+sendCon()
+
 
